a35ccda1ac3ab69e682dafcaef0e6d721599882f
envs <olaonipekun2001@yahoo.com>
2020-08-16 11:33:38 +0200

    Extra Component Features

--- meetapp/src/App.js
+++ meetapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router } from '@reach/router';
+import { Router, navigate } from '@reach/router';
 import firebase from './Firebase';
 
 import logo from './logo.svg';
@@ -24,18 +24,45 @@ class App extends Component {
     };
   }
 
+  componentDidMount () {
+    const ref = firebase.database().ref('user');
+
+    ref.on('value', snapshot => {
+      let FBUser = snapshot.val();
+      this.setState({ user: FBUser });
+    });
+  }
+
+  registerUser = userName => {
+    firebase.auth().onAuthStateChanged(FBUser => {
+      FBUser.updateProfile({
+        displayName: userName
+      }).then(() => {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+        navigate('/meetings');
+      });
+    });
+  };
+
   render() {
     return (
       <div>
         <Navigation user={this.state.user} />
         {this.state.user && (
-          <Welcome user = { this.state.user }/>
+          <Welcome user = { this.state.displayName }/>
         )}
         <Router>
           <Home path="/" user = { this.state.user }/>
           <Login path="/login" />
           <Meetings path="/meetings" />
-          <Register path="/register" />
+          <Register 
+            path="/register"
+            registerUser = {this.registerUser}
+          />
         </Router>
         
       </div>
